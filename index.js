c77b51d0bdace1306128041a6be261a543b81447
joseph Chun <jjunior.chun@gmail.com>
2019-01-18 01:25:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 const startingPoint = 42
 
 function distanceFromHqInBlocks (block) {
- return block - startingPoint
+ if (block < startingPoint) {
+    return startingPoint - block
+ } else {
+    return block - startingPoint
+}
 }
